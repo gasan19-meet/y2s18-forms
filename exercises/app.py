fca0761b88f519ad7eb63738cdc8a8a200b05d34
Gasan Ashhab <gasanashhab@gmail.com>
2018-08-08 18:30:02 +0300

    kdf

--- exercises/app.py
+++ exercises/app.py
@@ -1,10 +1,26 @@
 from databases import *
-from flask import Flask, render_template, url_for
+from flask import Flask,request, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+
     return render_template('home.html', students=query_all())
+
+
+@app.route('/add',methods=['GET', 'POST'])
+def new_student():
+	if request.method == 'GET':
+		return render_template('add.html')
+	elif request.method == 'POST':
+		print('received POST request')
+
+
+
+	return render_template('add.html')
+
+
+
 
 @app.route('/student/<int:student_id>')
 def display_student(student_id):
